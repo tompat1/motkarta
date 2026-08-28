8beb7ac520a91365d485bba77fd9c980596c302f
tompat1 <thomasrynell@me.com>
2026-08-28 14:46:19 +0200

    feat: update map tile source to OpenStreetMap and adjust active map control button styles

--- src/App.tsx
+++ src/App.tsx
@@ -5569,8 +5569,8 @@ function FoodMap({
       scrollWheelZoom: true,
     });
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 19,
     }).addTo(map);
 
